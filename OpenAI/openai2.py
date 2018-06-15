809e196bffbd4bba80abe5c9ff6e945507645d74
Tanmay Bhatnagar <tanmaybhatnagar84976@gmail.com>
2018-06-15 06:47:37 +0530

    Cartpole balance - move only base left/right

---
+++ OpenAI/openai2.py
@@ -0,0 +1,12 @@
+import gym
+env = gym.make('CartPole-v0')
+observation = env.reset()
+
+for _ in range(1000):
+    env.render()
+    cart_pos , cart_vel , pole_ang , ang_vel = observation
+    if pole_ang > 0:
+        action = 1
+    else:
+        action = 0
+    observation , reward , done , info = env.step(action)
